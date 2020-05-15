1a8c8a3c09eb67970e91d9a76a0b75f24de4221d
Robert Stok <rs.stok@gmail.com>
2020-05-15 13:12:35 +0100

    added a line in the Frame class that calculates the keypoints and descriptors for the image and stores them

--- video_stabilizer/frame.py
+++ video_stabilizer/frame.py
@@ -1,3 +1,6 @@
+import cv2
+
+
 class Frame:
     def __init__(self, frame):
         self.frame = frame
@@ -6,3 +9,8 @@ class Frame:
 
         self.global_x_offset = 0  # number of pixels offset from the left to the right, relative to the first frame
         self.global_y_offset = 0  # number of pixels offset from the left to the right, relative to the first frame
+
+        self.SIFT_filter = cv2.xfeatures2d.SIFT_create()
+
+        self.key_points, self.descriptors = self.SIFT_filter.detectAndCompute(self.frame, None)
+
